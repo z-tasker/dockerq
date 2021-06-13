80f01fae863f6de342d4d4bfd0dd86dcdc23d936
mgrasker <mgraskertheband@gmail.com>
2021-06-13 18:25:06 -0400

    try 4

--- serve.py
+++ serve.py
@@ -23,7 +23,7 @@ from starlette.templating import Jinja2Templates
 from middlewear import get_middlewear
 from log import get_logger
 
-MAX_DOCKER_CONCURRENCY = os.getenv("DOCKERQ_MAX_CONCURRENCY", 2)
+MAX_DOCKER_CONCURRENCY = int(os.getenv("DOCKERQ_MAX_CONCURRENCY", 2))
 
 
 async def home(request: Request) -> HTMLResponse:
